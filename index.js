06bdce75b7d7cc780fb07bb579da503bdea7260b
Deepak Fuloria <fuloriadeepak999@gmail.com>
2023-03-07 20:52:07 +0530

    basic test

--- index.js
+++ index.js
@@ -12,35 +12,37 @@ const PORT = process.env.PORT || 5000;
 // DB Connnection
 connect();
 // Express session middleware
-const store = new MongoDBStore({
-    uri: process.env.DB,
-    collection: 'sessions'
+// const store = new MongoDBStore({
+//     uri: process.env.DB,
+//     collection: 'sessions'
+// })
+// app.use(session({
+//     secret: process.env.SESSION_KEY,
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: {
+//         maxAge: 1000 * 7 * 24 * 60 * 60
+//     },
+//     store: store
+// }))
+// // Flash middleware 
+// app.use(flash())
+// app.use((req, res, next) => {
+//     res.locals.message = req.flash()
+//     next();
+// })
+// // Load static files
+// app.use(express.static("./views"));
+// app.use(express.urlencoded({ extended: true }));
+// // Set ejs
+// app.set("view engine", "ejs")
+// // Routes
+// app.use(userRoutes);
+// app.use(profileRoutes)
+// app.use(postRoutes)
+app.get("/",(req,res)=>{
+    res.send("done")
 })
-app.use(session({
-    secret: process.env.SESSION_KEY,
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 1000 * 7 * 24 * 60 * 60
-    },
-    store: store
-}))
-// Flash middleware 
-app.use(flash())
-app.use((req, res, next) => {
-    res.locals.message = req.flash()
-    next();
-})
-// Load static files
-app.use(express.static("./views"));
-app.use(express.urlencoded({ extended: true }));
-// Set ejs
-app.set("view engine", "ejs")
-// Routes
-app.use(userRoutes);
-app.use(profileRoutes)
-app.use(postRoutes)
-
 // Create server
 app.listen(PORT, () => {
     console.log(`Server running on port number: ${PORT}`);
