42882c8c7059bb0cda968dab350b37fffe4bdd3e
Deepak Fuloria <fuloriadeepak999@gmail.com>
2023-03-07 20:46:37 +0530

    test 1

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ const PORT = process.env.PORT || 5000;
 // DB Connnection
 connect();
 // Express session middleware
-// const store = new MongoDBStore({
-//     uri: process.env.DB,
-//     collection: 'sessions'
-// })
-// app.use(session({
-//     secret: process.env.SESSION_KEY,
-//     resave: true,
-//     saveUninitialized: true,
-//     cookie: {
-//         maxAge: 1000 * 7 * 24 * 60 * 60
-//     },
-//     store: store
-// }))
-// // Flash middleware 
-// app.use(flash())
-// app.use((req, res, next) => {
-//     res.locals.message = req.flash()
-//     next();
-// })
+const store = new MongoDBStore({
+    uri: process.env.DB,
+    collection: 'sessions'
+})
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 7 * 24 * 60 * 60
+    },
+    store: store
+}))
+// Flash middleware 
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.message = req.flash()
+    next();
+})
 // Load static files
 app.use(express.static("./views"));
 app.use(express.urlencoded({ extended: true }));
